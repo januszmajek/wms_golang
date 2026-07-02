aa729dddbaae4f74abf41d4f81d588f5d7bd030c
Janusz Majek <janusz.majek@capgemini.com>
2026-07-02 13:57:46 +0200

    pointers example

---
+++ pointers-example.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 42, 2701
+
+	p := &i         // wkaźnik do i
+	fmt.Println(*p) // przeczytaj i poprzez wkaźnik
+	*p = 21         // ustaw wartość i poprzez wkaźnik
+	fmt.Println(i)  // zobacz nową wartość i
+
+	p = &j         // wskaźnik do j
+	*p = *p / 37   // podziel j za pomocą wskaźnika
+	fmt.Println(j) // zobacz nowa wartość j
+}

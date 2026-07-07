523275e2901277f017a778fca9001edc6f8c46bf
Janusz Majek <janusz.majek@capgemini.com>
2026-07-07 12:44:10 +0200

    slices exercise
    Na początku źle zrozumiałem treść zadania. Myślałem, że dx i dy powinny
    być używane bezpośrednio do obliczania wartości elementów lub do
    tworzenia pojedynczego wycinka. Nie rozumiałem jeszcze, że dx i dy
    oznaczają wymiary obrazu: jego szerokość i wysokość.
    
    Na początku nie potrafiłem też stworzyć pustej tablicy result, ktora
    zawiera tyle wierszy ile wynosi dy i tyle kolumn ile wynosi wartość dx,
    ponownie wynikało to z braku zrozumienia treści zadania.
    
    Chciałem od razu wypełniać wycinki danymi, co było blłędne

---
+++ slices-exercise.go
@@ -0,0 +1,25 @@
+/*
+	Zaimplementuj funkcję Pic. Powinna ona zwrócić wycinek o długości dy, którego elementami są wycinki
+	zawierające dx 8-bitowych liczb całkowitych bez znaku (ang. 8-bit unisgned integer).
+	Gdy uruchomisz program, wyświetli on obrazek, interpretując wartości intów w skali szarości 
+ 	(tak naprawdę, to w skali „niebieskości”).
+
+	Wybór obrazka należy do ciebie. Przykładowe funkcje które dają ciekawy rezultat to: (x+y)/2, x*y, and x^y.
+
+	(Musisz użyć pętli by przypisać każdemu elementowi w [][]uint8 wartość typu []uint8.)
+
+	(Użyj uint8(intValue) by dokonać konwersji typów.)
+*/
+
+package main
+
+func Pic(dx, dy int) [][]uint8 {
+	var result [][]uint8
+	for y := 0; y < dy; y++ {
+		result = append(result, make([]uint8, dx))
+		for x := 0; x < dx; x++ {
+			result[y][x] = uint8((y + x) / 2)
+		}
+	}	
+	return result
+}

ff1bd3c09955b23eb1df51637bec87dfed87d67a
Janusz Majek <janusz.majek@capgemini.com>
2026-07-08 12:14:02 +0200

    funkcja domkniecia exercise-fibonnaci-clousure
    W Go domknięcie to funkcja, która "pamięta" zmienne z otaczającego ją
    zakresu nawet po zakończeniu działania funkcji zewnętrznej.
    
    1
    
    1

---
+++ exercise-fibonnaci-clousure.go
@@ -0,0 +1,52 @@
+package main
+
+// import "fmt"
+
+/*
+	Pobawmy się trochę funkcjami.
+	Zaimplementuj funkcję fibonacci która zwróci funkcję (domknięcie) która zwraca ciąg liczby Fibonacciego (0, 1, 1, 2, 3, 5, ...).
+ */
+
+// fibonacci to funkcja która zwraca
+// funkcję która zwraca int.
+func fibonacci() func() int {
+	numbers := []int{0}
+
+	return func() int {
+		if len(numbers) < 2 {
+			numbers = append(numbers, 1)
+			return 0
+		}
+		if len(numbers) == 2 {
+			numbers = append(numbers, 1)
+			return 1
+		}
+		if len(numbers) == 3 {
+			numbers = append(numbers, 1)
+			return 1
+		}
+		next := numbers[len(numbers)-1] + numbers[len(numbers)-2]
+		numbers = append(numbers, next)
+		return next
+	}
+}
+
+/* 
+	bardziej eleganckie
+func fibonacci() func() int {
+	a, b := 0, 1
+
+	return func() int {
+		result := a
+		a, b = b, a+b
+		return result
+	}
+}
+*/
+
+// func main() {
+// 	f := fibonacci()
+// 	for i := 0; i < 10; i++ {
+// 		fmt.Println(f())
+// 	}
+// }

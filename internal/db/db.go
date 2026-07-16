1b609168c0e42f53d84aa7dd7f287dd51d638f80
Janusz Majek <janusz.majek@capgemini.com>
2026-07-16 12:12:14 +0200

    use pgx insteqd of pq, update readme with podman commands
    (cherry picked from commit ec8da90f858ae30c48eda5ba28db0cf082768678)

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,25 +2,14 @@ package db
 
 import (
 	"database/sql"
-	"time"
 
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func Connect(databaseURL string) (*sql.DB, error) {
-	database, err := sql.Open("postgres", databaseURL)
+	db, err := sql.Open("pgx", databaseURL)
 	if err != nil {
 		return nil, err
 	}
-
-	database.SetMaxOpenConns(10)
-	database.SetMaxIdleConns(5)
-	database.SetConnMaxLifetime(time.Hour)
-
-	if err := database.Ping(); err != nil {
-		database.Close()
-		return nil, err
-	}
-
-	return database, nil
+	return db, nil
 }

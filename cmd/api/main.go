e1ac7f85b575a2aa04540f16985abd3aea6b0fdc
Janusz Majek <janusz.majek@capgemini.com>
2026-07-20 14:43:57 +0200

    database setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env Not found")
+	}
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		log.Fatal("Missing DATABASE_URL")
+	}
+
+	db, err := pgxpool.New(context.Background(), databaseURL)
+	if err != nil {
+		log.Fatalf("DB configuration error: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(context.Background()); err != nil {
+		log.Fatalf("db connection error: %v", err)
+	}
+
+	var databaseName string
+	err = db.QueryRow(
+		context.Background(),
+		"SELECT current_database()",
+	).Scan(&databaseName)
+	if err != nil {
+		log.Fatalf("Query error: %v", err)
+	}
+
+	log.Printf("Connected to db: %s", databaseName)
+}
